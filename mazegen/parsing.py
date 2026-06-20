4a6997ed208837708a169591268c3e5f3b48ddab
Sorin ROSU <sorinrosu.pro@gmail.com>
2026-06-21 00:31:50 +0200

    fix(errors): add try/excepts for error management in parsing

--- mazegen/parsing.py
+++ mazegen/parsing.py
@@ -9,16 +9,53 @@ class Parsing():
         self._algorythm: str = ""
 
     def parse(self, file: str) -> None:
+        errors: list = []
         with open(file, 'r') as f:
             content = f.read()
-        lines = [l for l in content.splitlines() if l.strip() and not l.startswith('#')]
-        params: dict = {key : value for key, value in (line.split('=') for line in lines)}
-        self._width = int(params['WIDTH'])
-        self._height = int(params['HEIGHT'])
-        x, y = (int(a) for a in params['ENTRY'].split(','))
-        self._entry = (x, y)
-        x, y = (int(a) for a in params['EXIT'].split(','))
-        self._exit = (x, y)
-        self._output_file = params['OUTPUT_FILE']
-        self._perfect = bool(params['PERFECT'] == True)
-        self._algorythm = params['ALGORYTHM']
+        lines: list = [l for l in content.splitlines() if
+                 l.strip() and not l.startswith('#')]
+        p: dict = {key : value for key, value in
+                   (line.split('=')for line in lines)}
+        try:
+            self._width = int(p['WIDTH'])
+            if self._width < 0:
+                errors.append(ValueError('width should not be negative'))
+        except ValueError as e:
+            errors.append(e)
+        try:
+            self._height = int(p['HEIGHT'])
+            if self._height < 0:
+                errors.append(ValueError('height should not be negative'))
+        except ValueError as e:
+            errors.append(e)
+        try:
+            x, y = (int(pos) for pos in p['ENTRY'].split(','))
+            if (x >= 0 and x < self._width) and (y >= 0 and y < self._height):
+                self._entry = (x, y)
+            else:
+                errors.append(ValueError('entry coordinates are out of bounds'))
+        except ValueError as e:
+            errors.append(e)
+        try:
+            x, y = (int(pos) for pos in p['EXIT'].split(','))
+            if (x >= 0 and x < self._width) and (y >= 0 and y < self._height):
+                self._exit = (x, y)
+            else:
+                errors.append(ValueError('exit coordinates are out of bounds'))
+        except ValueError as e:
+            errors.append(e)
+        self._output_file = p['OUTPUT_FILE']
+        if p['PERFECT'] == 'True' or p['PERFECT'] == 'False':
+            self._perfect = bool(p['PERFECT'] == True)
+        else:
+            errors.append(ValueError(f"Incorrect value '{p['PERFECT']}'"
+                        + " for variable 'PERFECT'"))
+        if p['ALGORYTHM'] == 'dfs' or p['ALGORYTHM'] == 'prims':
+            self._algorythm = p['ALGORYTHM']
+        else:
+            errors.append(f"Error: {p['ALGORYTHM']} algorythm not found")
+        if errors:
+            error: str = ''
+            for err in errors:
+                error += f'{type(err).__name__} : {str(err)}\n'
+            raise Exception(error)
