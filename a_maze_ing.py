e68e641c0bae394c3e465278da20d2da0463ed85
Sorin ROSU <sorinrosu.pro@gmail.com>
2026-06-21 14:44:03 +0200

    refactor(run): moved user input handler from generator.py to a_maze_ing.py & cleaned error management

--- a_maze_ing.py
+++ a_maze_ing.py
@@ -2,6 +2,89 @@ from mazegen.generator import MazeGenerator
 from mazegen.parsing import Parsing
 import sys
 
+def run() -> None:
+    maze = MazeGenerator(parsed)
+    try:
+        error = False
+        try:
+            maze._dfs()
+        except Exception as e:
+            raise (e)
+        maze._bfs(maze.entry, maze.exit)
+        maze.render()
+        while True:
+            while True:
+                show_hide = 'hide path' if maze._show else 'show path'
+                try:
+                    print(f'\n{maze._themes[maze._mode][0]}', end='')
+                    print('enter: regenerate maze')
+                    print(f'1) {show_hide}')
+                    print('2) maze theme')
+                    print('3) change wall type')
+                    print('-' * 10)
+                    print('q: quit')
+                    command = input('\nEnter command: ')
+                    break
+                except KeyboardInterrupt:
+                    error = True
+                    break
+            if error == True:
+                raise KeyboardInterrupt
+            if not command:
+                maze.generate()
+            elif command == '1':
+                maze._show = False if maze._show else True
+                maze.render()
+            elif command == '2':
+                print(maze._themes[maze._mode][2])
+                maze.theme_menu()
+                while True:
+                    try:
+                        print(maze._themes[maze._mode][0])
+                        theme_input = input(
+                            (' ' * 10) + 'Chose your theme: ')
+                        if not theme_input or theme_input == 'b':
+                            break
+                        if int(theme_input) <= 4:
+                            maze._mode = int(theme_input)
+                            break
+                        else:
+                            raise ValueError
+                    except ValueError:
+                        print('please enter valid input')
+                    except KeyboardInterrupt:
+                        raise KeyboardInterrupt
+                if theme_input == 'b':
+                    continue
+                elif not theme_input:
+                    maze.render()
+                    maze._mode = 0
+                else:
+                    maze.render()
+            elif command == '3':
+                while True:
+                    print('          ', end='')
+                    wall_input = input(
+                        'insert new wall type (enter for default): ')
+                    if wall_input == '':
+                        break
+                    elif len(wall_input) != 1:
+                        print('invalid wall type, ',
+                              'only one character is accepted')
+                        continue
+                    else:
+                        maze._wall = wall_input
+                        break
+                maze.render() 
+            elif command == 'q':
+                print('Program closed')
+                return
+            else:
+                print('please enter valid input')
+    except Exception as e:
+        print(e)
+    except KeyboardInterrupt:
+        print('\nCtrl+c detected: Program closed', end='')
 
 if __name__ == "__main__":
     if len(sys.argv) != 2:
@@ -12,10 +95,5 @@ if __name__ == "__main__":
         parsed.parse(sys.argv[1])
     except Exception as e:
         print(e, end='')
-        exit()
-    
-    maze = MazeGenerator(parsed)
-    try:
-        maze.generate()
-    except Exception as e:
-        print(e)
+        sys.exit()
+    run()
